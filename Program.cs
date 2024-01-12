16c404bccbd86eecee8bd1aac3a14f2ebd48b0e7
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-12 14:28:35 +0100

    Show Player card sum, and only first card of dealer

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@
 var playerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
 var dealerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
 
-Console.WriteLine($"Player got cards {string.Join(", ", playerCards)}");
-Console.WriteLine($"Dealer got cards {string.Join(", ", dealerCards)}");
+Console.WriteLine($"Player got cards {string.Join(", ", playerCards)}, adding up to {playerCards.Select(card => (int)card).Sum()}");
+Console.WriteLine($"Dealer got {dealerCards.Length} cards, first one is {dealerCards.First()}");

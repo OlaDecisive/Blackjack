4abb41be6e554ec19772af4bb07027b9f5b31437
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-12 14:42:30 +0100

    Add Hit/Stand command input

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
 ﻿Console.WriteLine("Blackjack!");
 
-var playerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
-var dealerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
+IEnumerable<Card> playerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
+IEnumerable<Card> dealerCards = Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 2);
 
-Console.WriteLine($"Player got cards {string.Join(", ", playerCards)}, adding up to {playerCards.Select(card => (int)card).Sum()}");
-Console.WriteLine($"Dealer got {dealerCards.Length} cards, first one is {dealerCards.First()}");
+Console.WriteLine($"Player cards: {string.Join(", ", playerCards)}, adding up to {playerCards.Select(card => (int)card).Sum()}");
+Console.WriteLine($"Dealer has {dealerCards.Count()} cards, first one is {dealerCards.First()}");
+
+Console.WriteLine($"[H]it, [S]tand?");
+
+var command = Console.ReadLine();
+
+if (command?.ToUpperInvariant().StartsWith("H") ?? false)
+{
+    playerCards = playerCards.Concat(Random.Shared.GetItems<Card>(Enum.GetValues<Card>(), 1));
+    Console.WriteLine($"Player cards: {string.Join(", ", playerCards)}, adding up to {playerCards.Select(card => (int)card).Sum()}");
+}
+else if (command?.ToUpperInvariant().StartsWith("S") ?? false)
+{
+    Console.WriteLine($"Player cards: {string.Join(", ", playerCards)}, adding up to {playerCards.Select(card => (int)card).Sum()}");
+}
+else
+{
+    Console.WriteLine("EH?");
+}

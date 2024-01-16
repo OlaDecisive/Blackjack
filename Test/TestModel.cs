f3e2b230da7c04836c353c52ddacc313a5745074
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-16 18:28:59 +0100

    Add more tests

--- Test/TestModel.cs
+++ Test/TestModel.cs
@@ -6,7 +6,7 @@ namespace Test;
 public class TestModel
 {
     [Fact]
-    public void TestNewGame()
+    public void TestCreateInitialGameState()
     {
         // Arrange
         var playerName = "tester";
@@ -29,5 +29,57 @@ public class TestModel
         var deserializedGameJson = JsonSerializer.Serialize<Game>(deserializedGame!);
 
         Assert.Equal(gameJson, deserializedGameJson);
+    }
+
+    [Fact]
+    public void TestCreateGame()
+    {
+        // Arrange
+        var playerName = "tester";
+        
+        // Act
+        var game = new Game("tester", new DoNothingShuffler());
+        
+        // Assert
+        Assert.True(game.Rounds.All(round => round.PlayerName == playerName));
+        Assert.Equal(52 - 2 - 2, game.Rounds.First().Deck.Cards.Count); // At start of game, two cards are dealt to player, two to dealer
+    }
+
+    [Fact]
+    public void TestAdvanceGameWithHit()
+    {
+        // Arrange
+        var playerName = "tester";
+        
+        // Act
+        var game = new Game("tester", new DoNothingShuffler());
+        game.Advance(PlayerDecision.Hit);
+
+        // Assert
+        Assert.Equal(2, game.Rounds.Count);
+        Assert.True(game.Rounds.All(round => round.PlayerName == playerName));
+        Assert.Equal(52 - 2 - 2, game.Rounds.First().Deck.Cards.Count); // At start of game, two cards are dealt to player, two to dealer
+        Assert.Equal(52 - 2 - 2 - 2, game.Rounds.Last().Deck.Cards.Count); // After one round, one extra card is dealt to the player, one to the dealer
+
+        Assert.Equal(GameStatus.Running, game.Status);
+        Assert.Contains("deck", game.GameDescription);
+        //Assert.Contains("dealer", game.DealersFinalHandDescription);
+    }
+
+    [Fact]
+    public void TestAdvanceGameWithStand()
+    {
+        // Arrange
+        var playerName = "tester";
+        var game = new Game("tester", new DoNothingShuffler());
+
+        // Act
+        game.Advance(PlayerDecision.Stand);
+
+        // Assert
+        Assert.Equal(2, game.Rounds.Count);
+        Assert.True(game.Rounds.All(round => round.PlayerName == playerName));
+        Assert.Equal(52 - 2 - 2, game.Rounds.First().Deck.Cards.Count); // At start of game, two cards are dealt to player, two to dealer
+        //Assert.Equal(52 - 2 - 2 - 2, game.Rounds.Last().Deck.Cards.Count); // After one round, one extra card is dealt to the player, one to the dealer
     }
 }

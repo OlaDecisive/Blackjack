96b1fe0b18d0a1a1f3b5e1686b6fb89954de1764
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-16 16:02:49 +0100

    Fix runTests script

--- runTests.bat
+++ runTests.bat
@@ -1,4 +1,4 @@
 dotnet test --collect:"XPlat Code Coverage"
-coverlet Test\bin\Debug\net8.0\Test.dll --target "dotnet" --targetargs "test Test/Test.csproj --no-build"
+coverlet Test\bin\Debug\net8.0\Test.dll --target "dotnet" --targetargs "test Test/Test.csproj --no-build" --format cobertura
 reportgenerator -reports:coverage.cobertura.xml -targetdir:"coveragereport" -reporttypes:Html
-start coveragereport\index.htm
+start coveragereport\index.html

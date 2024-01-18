029b3e1b7c49db5cf776881a4c8dc58a334cd18a
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-18 12:35:20 +0100

    Fix runTests.bat to generate coverage report for all test projects

--- runTests.bat
+++ runTests.bat
@@ -1,5 +1,3 @@
 dotnet test --collect:"XPlat Code Coverage"
-coverlet TestModel\bin\Debug\net8.0\TestModel.dll --target "dotnet" --targetargs "test TestModel/TestModel.csproj --no-build" --format cobertura
-coverlet TestWebApi\bin\Debug\net8.0\TestWebApi.dll --target "dotnet" --targetargs "test TestWebApi/TestWebApi.csproj --no-build" --format cobertura
-reportgenerator -reports:coverage.cobertura.xml -targetdir:"coveragereport" -reporttypes:Html
+reportgenerator -reports:**/coverage.cobertura.xml -targetdir:"coveragereport" -reporttypes:Html
 start coveragereport\index.html

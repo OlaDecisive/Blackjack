5c258efd1064b492f0ce96e0d8f945d4261d5290
Ola Østtveit <ola.osttveit@decisive.no>
2024-01-30 13:52:46 +0100

    Exclude database migrations from coverage report

--- runTests.bat
+++ runTests.bat
@@ -1,3 +1,3 @@
 dotnet test --collect:"XPlat Code Coverage"
-reportgenerator -reports:**/coverage.cobertura.xml -targetdir:"coveragereport" -reporttypes:Html
+reportgenerator -reports:**/coverage.cobertura.xml -targetdir:"coveragereport" -reporttypes:Html -classfilters:-Blackjack.Model.Migrations*
 start coveragereport\index.html
